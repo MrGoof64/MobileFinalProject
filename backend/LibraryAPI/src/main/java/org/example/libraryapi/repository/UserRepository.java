a55fe465c8c8f48146e93020da5087c155b86106
Aiden <fecalmattertastesreallygood@gmail.com>
2025-02-10 16:01:36 -0600

    Made some slight changes to the User functions

--- backend/LibraryAPI/src/main/java/org/example/libraryapi/repository/UserRepository.java
+++ backend/LibraryAPI/src/main/java/org/example/libraryapi/repository/UserRepository.java
@@ -2,5 +2,9 @@ package org.example.libraryapi.repository;
 
 import org.example.libraryapi.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
-public interface UserRepository extends MongoRepository<User, String> { }
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+}
